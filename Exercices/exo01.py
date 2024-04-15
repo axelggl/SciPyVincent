3d561b493f4a1cfa8984ea8571471187fe9202e0
maxel <axel.maerten@ynov.com>
2024-04-15 15:20:47 +0200

    Exercice 1

---
+++ Exercices/exo01.py
@@ -0,0 +1,10 @@
+# Utilisez scipy.io.loadmat pour lire un fichier MATLAB.mat fictif contenant les variables x et y. (Supposez que le fichier s'appelle data.mat). 
+
+import scipy.io as sio
+
+data = sio.loadmat('data.mat')
+x = data['x']
+y = data['y']
+
+print(x)
+print(y)
